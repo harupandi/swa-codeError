dc093fadb81b6984360b4833001d454eef0cb135
Bryan Pineda Gutierrez <brpineda@microsoft.com>
2023-12-13 13:22:00 -0700

    modified function message

--- api/message/index.js
+++ api/message/index.js
@@ -1,4 +1,4 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req) 
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res.json({
         text: "Hello from the API!"
